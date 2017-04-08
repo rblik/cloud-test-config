8e05d85c1b4ac84247c6f84c072f81704fc11ceb
Blik <rblick@bk.ru>
2017-04-09 00:04:46 +0300

    No.2

--- src/main/java/isr/ek0/CloudTestConfigApplication.java
+++ src/main/java/isr/ek0/CloudTestConfigApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class CloudTestConfigApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudTestConfigApplication.class, args);
 	}
