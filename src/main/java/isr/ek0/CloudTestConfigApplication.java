5dd47e2d3d278e86567a544a03f6631d075c6173
Blik <rblick@bk.ru>
2017-04-09 00:02:21 +0300

    No.1

---
+++ src/main/java/isr/ek0/CloudTestConfigApplication.java
@@ -0,0 +1,14 @@
+package isr.ek0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudTestConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestConfigApplication.class, args);
+	}
+}
